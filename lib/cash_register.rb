d11f3a1a20c0917bc2220fa4f57dc49208be4d61
Dean L Betty <deanlloyd64@gmail.com>
2019-09-19 23:49:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,8 +7,8 @@ class CashRegister
     @discount = discount
   end
   
-  def add_item(item, quantity = 1)
-    
+  def add_item(item, price, quantity = 1)
+    @total += (price * quantity)
   end
   
 end
